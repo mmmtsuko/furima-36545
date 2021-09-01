0747ff691f95b26409b437935446203b237a59e7
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-01 17:31:46 +0900

    devise/migrateファイル編集

--- db/migrate/20210901073450_devise_create_users.rb
+++ db/migrate/20210901073450_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_kana,     null: false
+      t.string :first_name_kana,    null: false
+      t.string :birthbay,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -42,3 +48,4 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     # add_index :users, :unlock_token,         unique: true
   end
 end
+
