c751b7711811188995af073434a29f6fe541bcfc
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-01 18:26:32 +0900

    バリテーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+ validates :nickname, precedence: true
+ validates :email, precedence:true, 
+ validates :encrypted_passwprd, precedence: true
+ validates :last_name, precedence: true
+ validates :first_name, precedence: true
+ validates :last_name_kana, precedence: true
+ validates :first_name_kana, precedence: true
+ validates :birthbay, precedence: true
+
+ has_many orders
+ has_many items
+ 
 end
