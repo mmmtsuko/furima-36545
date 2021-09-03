5bbcc425596b08f68997980607fb09fb54444b1f
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-03 17:05:32 +0900

    ログインと新規登録画面に遷移できるようになった

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,29 @@
 class ItemsController < ApplicationController
+
+
 def index
+ 
 end
 
 def  edit
 end  
 
 def create
+  
 end
 
 
 def update
-  if current_item.update(item_params)
-    redirect_to root_path
-  else
-    render :edit
-  end
+  current_item.update(item_params)
 end
-
 private
-
 def user_params
   params.require(:item).permit(:item_name, :text)
 end
-
-
-
-
 end
+
+
+
+
+
+
