05eb07c3eef1ff5751df06fbe1a56cfe673c2955
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-04 13:51:26 +0900

    ユーザーのログインできるようになった。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+  before_action :set_item, only: [:edit]
 
 def index
  
@@ -12,18 +12,32 @@ def create
   
 end
 
+def show
+end
 
 def update
-  current_item.update(item_params)
+  if current_user.update(user_params)
+    redirect_to root_path
+  else
+    render :edit
+  end
 end
+
 private
+
 def user_params
-  params.require(:item).permit(:item_name, :text)
+  params.require(:user).permit(:name, :email)
 end
+
+def move_to_index
+  unless user_signed_in?
+    redirect_to action: :index
+  end
 end
 
+def set_tweet
+  @item = Item.find(params[:id])
+end
 
-
-
-
+end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
  
  validates :nickname, presence: true
  
- validates :encrypted_password, presence: true, format: { with: /\A[a-z0-9]+\z/i}
+ validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
