9cb98b3c750dad57821d3b00b6784dfcb489eadd
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-06 16:44:05 +0900

    リファクタリングの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-     
- 
- validates :nickname, presence: true
- 
- validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
- validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
- validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
- validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
- validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
- 
- #has_many :orders
- #has_many :items
 
- validates :birthbay, presence: true
+     #has_many :orders
+     #has_many :items
+
  
- 
+ with_options presence: true do
+   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+   validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+   validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+   validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/}
+   validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/}
+   validates :birthbay
+   validates :nickname
+  end
 end
 
 #お名前(全角)は、名字と名前がそれぞれ必須であること。
