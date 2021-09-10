410cf457b6ef7d414ba19fbba43804da29b41eba
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-10 18:16:40 +0900

    テストコード終了

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name              { Faker::Name.initials(number: 5)}
+    text                   { Faker::Lorem.sentence }
+    category_id            {2}
+    sales_status_id        {2}
+    shipping_fee_id        {2}
+    prefecture_id          {2}
+    date_of_shipment_id    {2}
+    price                  {1000}
+    association :user
+  
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/item-sample.png'), filename: 'item-sample.png')
+    end
+
   end
 end
