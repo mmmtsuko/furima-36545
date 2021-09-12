aa8952eb683ada832a3c5a30057d58dda8965d1a
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-12 18:57:31 +0900

    変更できるようにコントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create]
- 
+  before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   
   def index 
    @item = Item.order("created_at DESC")
@@ -25,21 +25,27 @@ class ItemsController < ApplicationController
  end
 
  def edit
-  @item = Item.find(params[:id])
+   unless user_signed_in? && current_user.id == @item.user_id
+     redirect_to root_path
+   end
  end
 
- 
  def update
   if @item.update(item_params)
-    redirect_to item_path(@item.id)    
+    redirect_to item_path(@item.id)
   else
     render :edit
   end
 end
 
 
+
  private
 
+def set_item
+  @item = Item.find(params[:id])
+
+end
 
  def item_params
   params.require(:item).permit(:item_name, :text, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :date_of_shipment_id, :price, :image)
@@ -47,8 +53,7 @@ end
 end
 
 
-  
-
-end
+end 
+ 
 
 
