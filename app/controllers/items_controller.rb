35773cb6f90fb98e5430d0d31fa6667ac55f3a55
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-13 11:45:20 +0900

    destroyアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,10 @@ class ItemsController < ApplicationController
 end
 
 def destroy
-  item = Item.find(paerams[:id])
+  if user_signed_in? && current_user.id == @item.user_id
   item.destroy
+  redirect_to root_path
+  end
 end
 
 
