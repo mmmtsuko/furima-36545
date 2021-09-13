5ed27261b134d9043d4499e3c114c7edadf30210
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-13 17:45:30 +0900

    ストロングパラメーターの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,7 +16,8 @@ class OrdersController < ApplicationController
  private
 
  def order_params
-   params.require(:order).permit(:price)
+   params.require(:order).permit(:price).merge(token: params[:token])
  end  
 
 end
+
