15b276f11d4cd8c7ae5c54796b6eae68df7731d4
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-14 18:27:45 +0900

    formオブジェクト作成（アソシエーション記述）

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,7 +2,11 @@
 include Active Model::Model
 attr_accessor :postal_code, :prefecture_id, :city_name, :house_number_id, :building_name, :phone_number, :user_id, :item_id, :token
 
-validates :postal_code
+
+
+with_options presence: true do
+
+validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
 validates :prefecture_id
 validates :city_name
 validates :house_number_id
@@ -10,13 +14,12 @@ validates :building_name
 validates :user_id
 validates :item_id
 validates :token
-validates :phone_number
-
-
-
+validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Include hyphen(-)"}
+end
+validates :prefecture_id,numericality: {other_than: 1, message: "can't be blank"}
 
 def save
-
+  
 
 end
 
