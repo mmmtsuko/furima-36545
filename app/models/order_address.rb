84e3de74777a3661d20da92e2adb79ee2bf2f807
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-14 18:20:06 +0900

    formオブジェクト作成（コントローラーとアソシエーション）

---
+++ app/models/order_address.rb
@@ -0,0 +1,23 @@
+﻿class  OrderAddress
+include Active Model::Model
+attr_accessor :postal_code, :prefecture_id, :city_name, :house_number_id, :building_name, :phone_number, :user_id, :item_id, :token
+
+validates :postal_code
+validates :prefecture_id
+validates :city_name
+validates :house_number_id
+validates :building_name
+validates :user_id
+validates :item_id
+validates :token
+validates :phone_number
+
+
+
+
+def save
+
+
+end
+
+end
