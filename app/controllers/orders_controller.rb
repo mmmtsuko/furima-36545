0c7a741ed139b287b08212e399885c4be6673cc2
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-09-15 17:14:04 +0900

    コントローラーの記述修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
   before_action :set_item, only: [:index, :create]
 
 
@@ -11,13 +11,16 @@ class OrdersController < ApplicationController
 
  def create
        @order_private = OrderPrivate.new(order_params)
-   if  @order_private.valid?
-       @order_address.save
-     return redirect_to root_path
-   else
-      render action: :index
-   end
- end
+       if @order_private.valid?
+        pay_item
+        @order_private.save
+
+        return redirect_to root_path
+      else
+        render action: :index
+      end
+    end
+       
  
 
 private
@@ -27,7 +30,7 @@ def set_item
 end
 
  def order_params
-   params.require(:item_order).permit(:price, :postal_code, :prefecture_id, :city_name, :house_number_id, :building_name, :phone_number).merge(token: params[:token])
+   params.require(:order_private).permit(:price, :postal_code, :prefecture_id, :city_name, :house_number_id, :building_name, :phone_number).merge(token: params[:token])
  end
 end
  
